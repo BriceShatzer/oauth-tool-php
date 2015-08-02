a2db0247a910a65376fad1d902a27a65774c2cf2
Brice Shatzer <brice.shatzer@gmail.com>
2015-08-02 17:01:34 -0500

    Added missing Foundation dependencies via Component Installer and provided local file fallback functionality.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,13 +3,20 @@
 <head>
     <meta charset="utf-8">
     <title>OAuth Tool | Helps you create access tokens for your development environment</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/css/foundation.min.css" rel="stylesheet" />
+    <link href="/vendor/foundation/css/foundation.min.css" rel="stylesheet" />
     <style>section { padding: 3% 0; }</style>
     @yield('styles')
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/foundation.min.js"></script>
+    
+    <script>//-- local fallbacks if needed 
+    	window.jQuery || document.write('<script src="/vendor/jquery/jquery.min.js"><\/script>');
+    	window.Foundation || document.write('<script src="/vendor/js/foundation.min.js"><\/script>');
+    </script>
     @yield('scripts')
 </head>
 <body>
     @yield('content')
+    <script>$(document).foundation();</script>
 </body>
 </html>
