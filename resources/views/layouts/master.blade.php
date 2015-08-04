1b0a3f055a30209130475633b49f1797aeb5194f
Brice Shatzer <brice.shatzer@gmail.com>
2015-08-04 16:52:07 -0500

    Continued ui styling

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="utf-8">
     <title>OAuth Tool | Helps you create access tokens for your development environment</title>
+    <link href="/assets/styles.css" rel="stylesheet" />
     <link href="/vendor/foundation/css/foundation.min.css" rel="stylesheet" />
-    <style>section { padding: 3% 0; }</style>
     @yield('styles')
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/foundation.min.js"></script>
-    
     <script>//-- local fallbacks if needed 
     	window.jQuery || document.write('<script src="/vendor/jquery/jquery.min.js"><\/script>');
     	window.Foundation || document.write('<script src="/vendor/js/foundation.min.js"><\/script>');
--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -5,6 +5,7 @@
     <div class="row">
         <div class="small-12 columns">
             <h1>Begin OAuth Authentication Flow</h1>
+
             @if (count($errors) > 0 || session('message'))
             <div data-alert class="alert-box warning">
                 <ul>
@@ -17,6 +18,31 @@
                 </ul>
             </div>
             @endif
+
+            <ul class="tabs" data-tab>
+                <li class="tab-title active">
+                    <a href="#oauth1">
+                        <figure><img src="/assets/oauth-logo.png" alt="OAuth logo" /></figure>
+                        <h5>OAuth 1</h5>
+                    </a>
+                </li>
+                <li class="tab-title">
+                    <a href="#oauth2">
+                        <figure><img src="/assets/oauth-2-logo.png" alt="OAuth 2 logo" /></figure>
+                        <h5>OAuth 2</h5>
+                    </a>
+                </li>                
+            </ul>
+            <div class="tabs-content">
+                <div class="content active" id="oauth1">
+                    <h4>Yo dog, OAuth 1 content goes here</h4>
+                </div>
+                <div class="content" id="oauth2">
+                    <h4> OAuth 2 stuff goes here</h4>
+                </div>
+                
+            </div>
+
             <form method="post" action="{{ route('auth.redirect') }}">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <label>provider</label>
