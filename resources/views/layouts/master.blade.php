5d98814d8a292899379d16796d84ca12885c47ea
Brice Shatzer <brice.shatzer@gmail.com>
2015-08-04 22:04:54 -0500

    styling

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,15 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>OAuth Tool | Helps you create access tokens for your development environment</title>
-    <link href="/assets/styles.css" rel="stylesheet" />
+    <title>OAuth Tool | Helps you create access tokens for your development environment</title>    
     <link href="/vendor/foundation/css/foundation.min.css" rel="stylesheet" />
+    <link href="/assets/styles.css" rel="stylesheet" />
     @yield('styles')
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/foundation.min.js"></script>
     <script>//-- local fallbacks if needed 
     	window.jQuery || document.write('<script src="/vendor/jquery/jquery.min.js"><\/script>');
-    	window.Foundation || document.write('<script src="/vendor/js/foundation.min.js"><\/script>');
+    	window.Foundation || document.write('<script src="vendor/foundation/js/foundation.min.js"><\/script>');
     </script>
     @yield('scripts')
 </head>
--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -19,52 +19,63 @@
             </div>
             @endif
 
-            <ul class="tabs" data-tab>
-                <li class="tab-title active">
-                    <a href="#oauth1">
-                        <figure><img src="/assets/oauth-logo.png" alt="OAuth logo" /></figure>
-                        <h5>OAuth 1</h5>
-                    </a>
-                </li>
-                <li class="tab-title">
-                    <a href="#oauth2">
-                        <figure><img src="/assets/oauth-2-logo.png" alt="OAuth 2 logo" /></figure>
-                        <h5>OAuth 2</h5>
-                    </a>
-                </li>                
-            </ul>
-            <div class="tabs-content">
-                <div class="content active" id="oauth1">
-                    <h4>Yo dog, OAuth 1 content goes here</h4>
+            <div class="panel">
+                <ul class="tabs" data-tab>
+                    <li class="tab-title active">
+                        <a href="#oauth1">
+                            <figure><img src="/assets/oauth-logo.png" alt="OAuth logo" /></figure>
+                            <h5>OAuth 1</h5>
+                        </a>
+                    </li>
+                    <li class="tab-title">
+                        <a href="#oauth2">
+                            <figure><img src="/assets/oauth-2-logo.png" alt="OAuth 2 logo" /></figure>
+                            <h5>OAuth 2</h5>
+                        </a>
+                    </li>                
+                </ul>
+                <div class="tabs-content">
+
+                    <div class="content active" id="oauth1">
+                        <form method="post" action="{{ route('auth.redirect') }}">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <label>Provider</label>
+                            <select name="protocol_provider">
+                                @foreach ($protocols["oauth1"] as $provider)
+                                    <option value="{{$provider}}">{{$provider}}</option>
+                                @endforeach
+                            </select>  
+                            <label>client key</label>
+                            <input type="text" name="key" value="{{ old('key') }}" />
+                            <label>client secret</label>
+                            <input type="text" name="secret" value="{{ old('secret') }}" />
+                            <label>scopes (comma separated)</label>
+                            <input type="text" name="scopes" value="{{ old('scopes') }}" />
+                            <input type="submit" value="begin" class="button" />
+                        </form>                    
+                    </div>
+
+                    <div class="content" id="oauth2">
+                        <form method="post" action="{{ route('auth.redirect') }}">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <select name="protocol_provider">
+                                @foreach ($protocols["oauth2"] as $provider)
+                                    <option value="{{$provider}}">{{$provider}}</option>
+                                @endforeach   
+                            </select>
+                            <label>client key</label>
+                            <input type="text" name="key" value="{{ old('key') }}" />
+                            <label>client secret</label>
+                            <input type="text" name="secret" value="{{ old('secret') }}" />
+                            <label>scopes (comma separated)</label>
+                            <input type="text" name="scopes" value="{{ old('scopes') }}" />
+                            <input type="submit" value="begin" class="button" />
+                        </form>
+                    </div>    
                 </div>
-                <div class="content" id="oauth2">
-                    <h4> OAuth 2 stuff goes here</h4>
-                </div>
-                
+
             </div>
 
-            <form method="post" action="{{ route('auth.redirect') }}">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <label>provider</label>
-                <select name="protocol_provider">
-                    @foreach ($protocols as $group => $providers)
-                    @if (!empty($providers))
-                    <optgroup label="{{ $group }}">
-                    @foreach ($providers as $provider)
-                    <option value="{{ $group . ':' . $provider }}"@if (($group . ':' . $provider) == old('protocol_provider'))selected="selected"@endif>{{ $provider }}</option>
-                    @endforeach
-                    </optgroup>
-                    @endif
-                    @endforeach
-                </select>
-                <label>client key</label>
-                <input type="text" name="key" value="{{ old('key') }}" />
-                <label>client secret</label>
-                <input type="text" name="secret" value="{{ old('secret') }}" />
-                <label>scopes (comma separated)</label>
-                <input type="text" name="scopes" value="{{ old('scopes') }}" />
-                <input type="submit" value="begin" class="button" />
-            </form>
         </div>
     </div>
 </section>
